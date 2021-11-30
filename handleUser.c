a2862a300c3350b7e7bf9eee5c244fb07caa83af
t0hka1 <czk1263183073@outlook.com>
2021-11-30 13:09:12 +0800

    handleUser_function

---
+++ handleUser.c
@@ -0,0 +1,23 @@
+// 此处添加头文件
+// 待添加其他头文件
+#include "handleUser.h"
+
+void sign_up(){     //equal add normal_user
+
+}   
+
+void sign_in(){
+
+}
+
+void list_user(){
+
+}
+
+void del_user(){
+
+}
+
+void add_system_user(){   //super_user have the permission
+
+}   
