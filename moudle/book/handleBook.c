056b492a917ca8aa18afd2a1c9dcf56e8ec2f856
t0hka1 <czk1263183073@outlook.com>
2021-12-04 22:08:24 +0800

    handleBook:add nothing

---
+++ moudle/book/handleBook.c
@@ -0,0 +1,17 @@
+// 此处添加头文件
+// 待添加其他头文件
+#include "handleBook.h"
+
+void listBooks();
+
+void modifyBook();
+
+void delBook();
+
+void addBook();
+
+void borrowBook();
+
+void returnBook();
+
+void searchBook();
