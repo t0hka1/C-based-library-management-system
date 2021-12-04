d504e8926256bbe848ec723cc8ab3d8333640baf
t0hka1 <czk1263183073@outlook.com>
2021-12-04 18:28:08 +0800

    handleUser_function:add signup details

--- handleUser.c
+++ handleUser.c
@@ -2,8 +2,20 @@
 // 待添加其他头文件
 #include "handleUser.h"
 
-void sign_up(){     //equal add normal_user
+int check_repeat_ID();
 
+
+void sign_up(){     // add normal_user
+    while (1){
+        printf("请输入你想注册的ID:");
+        
+        // 如果已注册
+        if (check_repeat_ID()){
+            
+        }else{
+
+        }
+    }
 }   
 
 void sign_in(){
@@ -21,3 +33,5 @@ void del_user(){
 void add_system_user(){   //super_user have the permission
 
 }   
+
+ 
